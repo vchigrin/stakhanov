af4ff06974ad7013e588d5241c47a10d0a9ffbfe
Vyacheslav Chigrin <vchigrin@yandex-team.ru>
2016-01-05 01:23:25 +0300

    Generate .bat files for invoking ninja in proper environment.
    CMake calls link through it internal wrapper, that calls to rc.exe.
    We have to provide correct PATH value so it will be able locate this executable.

--- src/build/run_cmake.py
+++ src/build/run_cmake.py
@@ -12,10 +12,31 @@ CMAKE_EXECUTABLE = os.path.join(CMAKE_DIR, 'bin', 'cmake.exe')
 OUT_DIR = os.path.join(SRC_DIR, 'out')
 
 
-def gen_for_build_type(build_type, folder_suffix):
+def generate_build_bat(dest_dir, msvs_bat_file_command):
+    lines = []
+    # Save current dir and cd to build dir
+    lines.append('pushd %~dp0')
+    # Use local environment
+    lines.append('setlocal')
+    # Set up environment
+    lines.append('call "{}" {}'.format(
+        msvs_bat_file_command[0], ' '.join(msvs_bat_file_command[1:])))
+    # Call ninja, passing it whatever user passed to .bat file.
+    lines.append('ninja %*')
+    # Restore environment
+    lines.append('endlocal')
+    # Restore directory
+    lines.append('popd')
+    dest_path = os.path.join(dest_dir, 'build.bat')
+    with open(dest_path, 'w') as f:
+        f.write('\n'.join(lines))
+
+
+def gen_for_build_type(build_type, folder_suffix, msvs_bat_file_command):
     dest_dir = os.path.join(OUT_DIR, build_type + folder_suffix)
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
+    generate_build_bat(dest_dir, msvs_bat_file_command)
     os.chdir(dest_dir)
     args = [
         CMAKE_EXECUTABLE,
@@ -26,10 +47,9 @@ def gen_for_build_type(build_type, folder_suffix):
     subprocess.check_call(args)
 
 
-def grab_environ(bat_path, platform_type):
-    args = [
-        bat_path, platform_type, '&', 'set'
-    ]
+def grab_environ(msvs_bat_file_command):
+    args = list(msvs_bat_file_command)
+    args.extend(['&', 'set'])
     output = subprocess.check_output(args)
     for line in output.split('\r'):
         line = line.strip()
@@ -41,9 +61,10 @@ def grab_environ(bat_path, platform_type):
 
 def gen_for_platform(vs_dir, platform_type):
     bat_path = os.path.join(vs_dir, 'VC', 'vcvarsall.bat')
-    grab_environ(bat_path, platform_type)
-    gen_for_build_type('Debug', '_' + platform_type)
-    gen_for_build_type('Release', '_' + platform_type)
+    msvs_bat_file_command = [bat_path, platform_type]
+    grab_environ(msvs_bat_file_command)
+    gen_for_build_type('Debug', '_' + platform_type, msvs_bat_file_command)
+    gen_for_build_type('Release', '_' + platform_type, msvs_bat_file_command)
 
 
 def main():
