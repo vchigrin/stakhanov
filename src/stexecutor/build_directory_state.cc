2e5654e7b1295f79ab38d94d76b3272508f0fb72
Vyacheslav Chigrin <vchigrin@yandex-team.ru>
2016-05-31 20:56:03 +0300

    Shring scope of lock in BuildDirectoryState.

--- src/stexecutor/build_directory_state.cc
+++ src/stexecutor/build_directory_state.cc
@@ -23,50 +23,7 @@ namespace {
 log4cplus::Logger logger_ = log4cplus::Logger::getInstance(
     L"BuildDirectoryState");
 
-}  // namespace
-
-BuildDirectoryState::BuildDirectoryState(
-    const boost::filesystem::path& dir_path)
-    : build_dir_path_(base::WideToLower(dir_path.string<std::wstring>())) {
-}
-
-std::string BuildDirectoryState::GetFileContentId(
-    const boost::filesystem::path& rel_path) const {
-  std::lock_guard<std::mutex> lock(instance_lock_);
-  auto it_cache = content_id_cache_.find(rel_path);
-  if (it_cache != content_id_cache_.end()) {
-    return it_cache->second;
-  }
-  boost::filesystem::path abs_path = build_dir_path_ / rel_path;
-  CryptoPP::Weak::MD5 hasher;
-  if (!HashFileContent(abs_path, &hasher)) {
-    LOG4CPLUS_ERROR(
-        logger_, "Failed hash file " << abs_path.generic_string().c_str());
-    return std::string();
-  }
-  std::vector<uint8_t> digest(hasher.DigestSize());
-  hasher.Final(&digest[0]);
-  std::string result = base::BytesToHexString(digest);
-  content_id_cache_.insert(std::make_pair(rel_path, result));
-  return result;
-}
-
-bool BuildDirectoryState::TakeFileFromStorage(
-    FilesStorage* files_storage,
-    const std::string& storage_id,
-    const boost::filesystem::path& rel_path) {
-  std::lock_guard<std::mutex> lock(instance_lock_);
-  boost::filesystem::path abs_path = build_dir_path_ / rel_path;
-  content_id_cache_.erase(rel_path);
-  if (!files_storage->GetFileFromStorage(storage_id, abs_path))
-    return false;
-  // Take advantage of the fact that FilesystmFilesStorage uses
-  // same hash algorithm as we do.
-  // TODO(vchigrin): Make thie assumption clearer.
-  content_id_cache_.insert(std::make_pair(rel_path, storage_id));
-  // We must update modification time of file (files storage may not do this
-  // if it uses hard links). Without it some commands,
-  // like used by NaCL toolcain in chromium build, will fail.
+bool UpdateModificationTime(const boost::filesystem::path& abs_path) {
   // boost::filesystem::last_write_time uses std::time_t, that is measured in
   // seconds. Such granularity is not always enough
   base::ScopedHandle file_handle(
@@ -103,13 +60,67 @@ bool BuildDirectoryState::TakeFileFromStorage(
   return true;
 }
 
-void BuildDirectoryState::RemoveFile(const boost::filesystem::path& rel_path) {
+}  // namespace
+
+BuildDirectoryState::BuildDirectoryState(
+    const boost::filesystem::path& dir_path)
+    : build_dir_path_(base::WideToLower(dir_path.string<std::wstring>())) {
+}
+
+std::string BuildDirectoryState::GetFileContentId(
+    const boost::filesystem::path& rel_path) const {
   std::lock_guard<std::mutex> lock(instance_lock_);
+  auto it_cache = content_id_cache_.find(rel_path);
+  if (it_cache != content_id_cache_.end()) {
+    return it_cache->second;
+  }
+  boost::filesystem::path abs_path = build_dir_path_ / rel_path;
+  CryptoPP::Weak::MD5 hasher;
+  if (!HashFileContent(abs_path, &hasher)) {
+    LOG4CPLUS_ERROR(
+        logger_, "Failed hash file " << abs_path.generic_string().c_str());
+    return std::string();
+  }
+  std::vector<uint8_t> digest(hasher.DigestSize());
+  hasher.Final(&digest[0]);
+  std::string result = base::BytesToHexString(digest);
+  content_id_cache_.insert(std::make_pair(rel_path, result));
+  return result;
+}
+
+bool BuildDirectoryState::TakeFileFromStorage(
+    FilesStorage* files_storage,
+    const std::string& storage_id,
+    const boost::filesystem::path& rel_path) {
+  boost::filesystem::path abs_path = build_dir_path_ / rel_path;
+  bool is_ok = files_storage->GetFileFromStorage(storage_id, abs_path);
+  if (is_ok) {
+    // We must update modification time of file (files storage may not do this
+    // if it uses hard links). Without it some commands,
+    // like used by NaCL toolcain in chromium build, will fail.
+    is_ok &= UpdateModificationTime(abs_path);
+  }
+
+  std::lock_guard<std::mutex> lock(instance_lock_);
+  content_id_cache_.erase(rel_path);
+  if (!is_ok)
+    return false;
+  // Take advantage of the fact that FilesystmFilesStorage uses
+  // same hash algorithm as we do.
+  // TODO(vchigrin): Make thie assumption clearer.
+  content_id_cache_.insert(std::make_pair(rel_path, storage_id));
+  return true;
+}
+
+void BuildDirectoryState::RemoveFile(const boost::filesystem::path& rel_path) {
+  {
+    std::lock_guard<std::mutex> lock(instance_lock_);
+    content_id_cache_.erase(rel_path);
+  }
   boost::filesystem::path abs_path = build_dir_path_ / rel_path;
   boost::system::error_code remove_error;
   // Delete old file, if any.
   boost::filesystem::remove(abs_path, remove_error);
-  content_id_cache_.erase(rel_path);
 }
 
 void BuildDirectoryState::NotifyFileChanged(
